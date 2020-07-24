2e178e6e59c8bec94c1b4b647c4d26f5bbcf0b70
ASHLESHA SHIRGUPPE <ashlesha85@gmail.com>
2020-07-24 19:30:20 +0530

    Email Template Module

---
+++ LMSBL/Repository/EmailTemplateRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LMSBL.DBModels;
+using System.Data;
+using System.Data.SqlClient;
+using LMSBL.Common;
+using System.IO;
+using System.Configuration;
+using System.Web.UI;
+
+namespace LMSBL.Repository
+{
+    public class EmailTemplateRepository
+    {
+        DataRepository db = new DataRepository();
+        Exceptions newException = new Exceptions();
+
+        public List<tblTemplateAssign> GetEmailTemplateAssigns(int TenantId)
+        {
+            try 
+            {
+                db.parameters.Clear();
+                db.AddParameter("@tenantId", SqlDbType.Int, TenantId);
+                DataSet ds = db.FillData("sp_GetEmailTemplate");
+                List<tblTemplateAssign> Emailtemplate = ds.Tables[0].AsEnumerable().Select(dr => new tblTemplateAssign
+                {
+                    TenantId = Convert.ToInt32(dr["TenantId"]),
+                    TemplateId = Convert.ToInt32(dr["TemplateId"]),
+                    EmailSubject= Convert.ToString(dr["EmailSubject"]),
+                    EmailBody = Convert.ToString(dr["EmailBody"]),
+                    DateCreated = Convert.ToDateTime(dr["DateCreated"])
+                
+
+                  }).ToList();
+                    return Emailtemplate;
+
+            }
+            catch(Exception ex)
+            {
+
+                newException.AddException(ex);
+                throw ex;
+            }
+        
+        
+        }
+
+        public List<tblTemplateAssign> GetEmailTemplateAssignsById(int TemplateId)
+        {
+            try
+            {
+                db.parameters.Clear();
+                db.AddParameter("@templateId", SqlDbType.Int, TemplateId);
+                DataSet ds = db.FillData("sp_GetEmailTemplateById");
+                List<tblTemplateAssign> Emailtemplate = ds.Tables[0].AsEnumerable().Select(dr => new tblTemplateAssign
+                {
+                    TenantId = Convert.ToInt32(dr["TenantId"]),
+                    TemplateId = Convert.ToInt32(dr["TemplateId"]),
+                    EmailSubject = Convert.ToString(dr["EmailSubject"]),
+                    EmailBody = Convert.ToString(dr["EmailBody"]),
+                    DateCreated = Convert.ToDateTime(dr["DateCreated"])
+
+
+                }).ToList();
+                return Emailtemplate;
+
+            }
+            catch (Exception ex)
+            {
+
+                newException.AddException(ex);
+                throw ex;
+            }
+
+
+        }
+
+    }
+}
---
+++ LMSWeb/Controllers/EmailTemplateController.cs
@@ -0,0 +1,57 @@
+﻿using System.Configuration;
+using System.Net.Configuration;
+using System.Net.Mail;
+using System.Web.Mvc;
+using LMSBL.Common;
+using LMSBL.DBModels;
+using LMSBL.Repository;
+using LMSWeb.App_Start;
+using LMSWeb.ViewModel;
+using System.Linq;
+using System.Web.Script.Serialization;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System;
+
+
+
+namespace LMSWeb.Controllers
+{
+    public class EmailTemplateController : Controller
+    {
+        EmailTemplateRepository etr = new EmailTemplateRepository();
+        Exceptions newException = new Exceptions();
+        // GET: EmailTemplate
+        public ActionResult Index()
+        {
+            TblUser sessionUser = (TblUser)Session["UserSession"];
+            var lstTemplate = etr.GetEmailTemplateAssigns(sessionUser.TenantId);
+            return View(lstTemplate);
+
+        }
+        public ActionResult EditTemplate(int id)
+        {
+            try
+            {
+                List<tblTemplateAssign> TemplateDetails = new List<tblTemplateAssign>();
+                TemplateDetails = etr.GetEmailTemplateAssignsById(id);
+
+                return View("UpdateEmailTemplate", TemplateDetails[0]);
+            }
+            catch (Exception ex)
+            {
+                newException.AddException(ex);
+                TempData["Message"] = "-1";
+                return RedirectToAction("Index");
+
+            }
+
+        }
+    }
+}
