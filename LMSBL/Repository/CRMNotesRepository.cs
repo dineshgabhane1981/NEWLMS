3536993b9ceac7218994a3243e088e853ac192af
ashlesha85 <68151808+ashlesha85@users.noreply.github.com>
2020-10-08 16:14:11 +0530

    File Notes functionality added

---
+++ LMSBL/Repository/CRMNotesRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMSBL.Common;
+using LMSBL.DBModels.CRMNew;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+
+namespace LMSBL.Repository
+{
+   public class CRMNotesRepository
+    {
+        Exceptions newException = new Exceptions();
+        public List<SelectListItem> GetClient(int ClientId)
+        {
+            List<SelectListItem> lstCRMclient = new List<SelectListItem>();
+            
+            List<tblCRMUser> lstCRMUsers = new List<tblCRMUser>();
+            using (var context = new CRMContext())
+            {
+                lstCRMUsers = context.tblCRMUsers.Where(a => a.ClientId == ClientId && a.CurrentStage == 3).ToList();
+            }
+            foreach(var user in lstCRMUsers)
+            {
+                lstCRMclient.Add(new SelectListItem
+                {
+                    Text = Convert.ToString(user.FirstName +" " + user.LastName),
+                    Value = Convert.ToString(user.Id)
+                });
+            }
+           
+            return lstCRMclient;
+        }
+
+        public bool SaveUserData(tblCRMNote objCRMNote)
+        {
+            bool status = false;
+            using (var context = new CRMContext())
+            {
+                context.Database.Log = Console.Write;
+                using (DbContextTransaction transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        //var address = context.tblCRMUsers.First(a => a.Id == 2);
+                        context.tblCRMNotes.Add(objCRMNote);
+                        context.SaveChanges();
+
+                        
+
+                        transaction.Commit();
+                        status = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        newException.AddException(ex);
+                        throw ex;
+                    }
+
+                }
+            }
+
+            return status;
+        }
+     }
+}
--- LMSWeb/Controllers/FileNotesController.cs
+++ LMSWeb/Controllers/FileNotesController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using LMSBL.Common;
+using LMSBL.DBModels.CRMNew;
+using LMSBL.DBModels;
+using LMSBL.Repository;
+using LMSWeb.ViewModel;
 
 namespace LMSWeb.Controllers
 {
     public class FileNotesController : Controller
     {
         // GET: FileNotes
+        CRMNotesRepository crmnr = new CRMNotesRepository();
         public ActionResult Index()
         {
-            return View();
+            //  tblCRMUser objclient = new tblCRMUser();
+            CRMNotesViewModel CRMNotesView = new CRMNotesViewModel();
+            TblUser sessionUser = (TblUser)Session["UserSession"];
+            var lstclient = crmnr.GetClient(Convert.ToInt32(sessionUser.CRMClientId));
+            CRMNotesView.lstCRMclient = lstclient;
+            //objclient.ClientList = lstclient;
+            return View(CRMNotesView);
+
+           // return View();
+        }
+
+        public bool AddNotes(CRMNotesViewModel cRMNotesViewModel)
+        {
+            TblUser sessionUser = (TblUser)Session["UserSession"];            
+            cRMNotesViewModel.objCRMnotes.CreatedDate = DateTime.Now;
+            cRMNotesViewModel.objCRMnotes.CreatedBy = sessionUser.UserId;
+            var status = crmnr.SaveUserData(cRMNotesViewModel.objCRMnotes);
+
+
+            return status;
         }
     }
 }
