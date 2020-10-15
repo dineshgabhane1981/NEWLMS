191a12689b4bdbdc3bce95e05d0113990e4fc196
Dinesh <dinesh.gabhane@hotmail.com>
2020-10-15 22:11:23 +0530

    few changes

--- LMSWeb/Controllers/InvoiceController.cs
+++ LMSWeb/Controllers/InvoiceController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using LMSBL.Common;
+using LMSBL.DBModels.CRMNew;
+using LMSBL.DBModels;
+using LMSBL.Repository;
+using LMSWeb.ViewModel;
+using System.Threading;
 
 namespace LMSWeb.Controllers
 {
     public class InvoiceController : Controller
     {
+        CRMNotesRepository crmnr = new CRMNotesRepository();
+        CRMUsersRepository crmUsersRepository = new CRMUsersRepository();
         // GET: Invoice
         public ActionResult Index()
         {
-            return View();
+            CRMInvoiceViewModel CRMInvoiceModelView = new CRMInvoiceViewModel();
+            tblCRMInvoice objNewInvoice = new tblCRMInvoice();
+            CRMInvoiceModelView.ObjCRMInvoivce = objNewInvoice;
+            TblUser sessionUser = (TblUser)Session["UserSession"];
+            CRMInvoiceModelView.lstCRMclient = crmnr.GetClient(Convert.ToInt32(sessionUser.CRMClientId));
+            return View(CRMInvoiceModelView);
         }
     }
 }
---
+++ LMSWeb/ViewModel/CRMInvoiceViewModel.cs
@@ -0,0 +1,19 @@
+﻿using LMSBL.DBModels.CRMNew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMSWeb.ViewModel
+{
+    public class CRMInvoiceViewModel
+    {
+        public tblCRMInvoice ObjCRMInvoivce { get; set; }
+        public List<tblCRMInvoiceItem> ObjCRMInvoiceItemLST { get; set; }
+        public List<SelectListItem> lstCRMclient { get; set; }
+        
+        public string JsonData { get; set; }
+        public int Client { get; set; }
+    }
+}
